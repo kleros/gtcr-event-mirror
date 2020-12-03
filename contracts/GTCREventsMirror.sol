4ef70b63f8ee8f3dec54d488a1c17d10a563affa
mtsalenc <mtsalenc@gmail.com>
2020-12-03 19:30:09 -0300

    feat: add accidental event replay protection

--- contracts/GTCREventsMirror.sol
+++ contracts/GTCREventsMirror.sol
@@ -16,6 +16,13 @@ contract GTCREventsMirror {
   address public oracle;
   address public admin; // Note that is not the proxy administrator.
   bool public initialized;
+  mapping(uint => uint) public eventToCount; // Maps an event identifier to the number of events logged. Used to prevent accidental replays.
+
+  // Enums and immutable variables cannot be used with upgradable smart contracts without complexity,
+  // so we just use regular variables.
+  // See https://docs.openzeppelin.com/upgrades-plugins/1.x/faq#why-cant-i-use-custom-types.
+  // and https://docs.openzeppelin.com/upgrades-plugins/1.x/faq#why-cant-i-use-custom-types
+  uint public ITEM_STATUS_CHANGE_IDENTIFIER; // Intentionally kept at 0.
 
   /**
    *  @dev Emitted when a party makes a request, raises a dispute or when a request is resolved.
@@ -62,20 +69,32 @@ contract GTCREventsMirror {
     admin = _newAdmin;
   }
 
-  /** @dev Relay an item status chain event.
-   *  @param _itemID The ID of the affected item.
-   *  @param _requestIndex The index of the request.
-   *  @param _roundIndex The index of the round.
-   *  @param _disputed Whether the request is disputed.
-   *  @param _resolved Whether the request is executed.
+  /** @dev Relay an item status change events. Items MUST be submitted in the order they were logged in the source chain.
+   *  @param _itemIDs The IDs of the affected items.
+   *  @param _requestIndexes The indexes of the requests.
+   *  @param _roundIndexes The indexes of the rounds.
+   *  @param _disputesCreated Whether the requests are disputed.
+   *  @param _requestsResolved Whether the request are resolved.
+   *  @param _eventCountStart The event count to start logging. Used to prevent accidental duplicate events.
    */
   function emitItemStatusChange(
-    bytes32 _itemID,
-    uint _requestIndex,
-    uint _roundIndex,
-    bool _disputed,
-    bool _resolved
+    bytes32[] memory _itemIDs,
+    uint[] memory _requestIndexes,
+    uint[] memory _roundIndexes,
+    bool[] memory _disputesCreated,
+    bool[] memory _requestsResolved,
+    uint _eventCountStart
   ) external onlyOracle afterInitialized {
-    emit ItemStatusChange(_itemID, _requestIndex, _roundIndex, _disputed, _resolved);
+    for (uint i = 0; i < _itemIDs.length; i++) {
+      require(_eventCountStart == eventToCount[ITEM_STATUS_CHANGE_IDENTIFIER], "Event already emmited.");
+      emit ItemStatusChange(
+        _itemIDs[0],
+        _requestIndexes[0],
+        _roundIndexes[0],
+        _disputesCreated[0],
+        _requestsResolved[0]
+      );
+      eventToCount[ITEM_STATUS_CHANGE_IDENTIFIER]++;
+    }
   }
 }
